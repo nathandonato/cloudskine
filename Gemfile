a62e34088ba4c6ba005fc3da4f314cfd855d0af4
Nathan Donato <nathanjdonato@gmail.com>
2019-09-26 11:18:15 -0400

    Add UserSerializer

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'puma', '~> 3.11'
 gem 'bcrypt', '~> 3.1.7'
 gem 'jwt'
 
+gem 'active_model_serializers', '~> 0.10.0'
 gem 'memoist'
 
 # Use Active Storage variant
---
+++ test/serializers/user_serializer_test.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'jwt_manager'
+
+class UserSerializerTest < ActiveSupport::TestCase
+  setup do
+    @user = users(:one)
+  end
+
+  test 'serializes user' do
+    actual = UserSerializer.new(@user).as_json
+
+    assert_equal @user.id, actual[:id]
+    assert_equal @user.username, actual[:username]
+    assert_equal @user.email, actual[:email]
+    assert_nil actual[:password]
+    assert_nil actual[:password_digest]
+  end
+end
