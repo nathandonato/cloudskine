7ad62cb9ad741df2f81ef06f09bd2a97ec804709
Nathan Donato <nathanjdonato@gmail.com>
2019-10-23 15:18:48 -0400

    Add Logout endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'login', to: 'authentication#login'
+      delete 'logout', to: 'authentication#logout'
 
       resources :users, only: :create
       resources :entries, except: :new
